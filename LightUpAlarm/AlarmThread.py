c5f12094be1703e5c8ac6aa69ddb89046786082d
carlosperate <carlosperate@users.noreply.github.com>
2015-03-13 17:14:06 +0000

    Add AlarmThread class and unit test. This class is used to launch a thread per running alarm, to trigger an alert when the alarm time has been reached.

---
+++ LightUpAlarm/AlarmThread.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python2
+#
+# Class to have alarms running on their own threads.
+#
+# Copyright (c) 2015 carlosperate https://github.com/carlosperate/
+# Licensed under The MIT License (MIT), a copy can be found in the LICENSE file
+#
+# This file only contains a class definition, which description can be found in
+# its docstring.
+#
+from __future__ import unicode_literals, absolute_import, print_function
+import sys
+import time
+import threading
+
+
+class AlarmThread(threading.Thread):
+    """
+    This thread class contains an instance to an AlarmItem and when it is
+    running checks the time every 5 seconds to determine if the Alarm should be
+    triggered.
+
+    This class does NOT edit the AlarmItem instance it takes, it only uses its
+    data to determine if the alarm is meant to be trigger or not. If it reads
+    old data while it is being modified by AlarmManger, it will read the new
+    correct version at the next infinite loop iteration and not cause a problem.
+    It is for this reason that locks are not required for the AlarmItem data or
+    the __run private variable (modifiable by the externally accessible stop()
+    method).
+    """
+    #
+    # metaclass methods
+    #
+    def __init__(self, alarm_item, alarm_callback):
+        """
+        AlarmThread constructor. Takes an AlarmItem instance and a callback
+        function to initialise the member variables.
+        :param alarm_item: AlarmItem instance.
+        :param alarm_callback: Callback function to execute when alarm triggers.
+        """
+        threading.Thread.__init__(self)
+        self.daemon = True
+
+        self.__alarm = alarm_item
+        self.__alarm_callback = alarm_callback
+        self.__id = self.__alarm.id_
+        self.__run = True
+
+    #
+    # control thread methods
+    #
+    def run(self):
+        """
+        Infinite loop function to run until it is stopped by calling the stop()
+        method. It sleeps for 5 seconds between iterations.
+        At each iteration it first determines if the alarm is active and set to
+        trigger any day, if so it then checks if this time is the alarm time.
+        """
+        while self.__run:
+            # If any of these conditions is false there is no alarm to alert
+            if (self.__alarm.active is True) and \
+               (self.__alarm.any_active_day() is True):
+                # Check if it is the alarm time
+                time_now = time.localtime(time.time())
+                if (self.__alarm.repeat[time_now.tm_wday] is True) and \
+                   (self.__alarm.hour == time_now.tm_hour) and \
+                   (self.__alarm.minute == time_now.tm_min):
+                    self.__alarm_callback()
+                    # Wait for the current minute to be over, in order to not
+                    # execute the callback more than once
+                    while (self.__alarm.hour == time_now.tm_hour) and \
+                          (self.__alarm.minute == time_now.tm_min):
+                        time.sleep(1)
+                        time_now = time.localtime(time.time())
+
+            # Now just sleep for 5 seconds before checking again
+            time.sleep(5)
+
+            # debugging test
+            #print('Alarm %s %s %02d:%02d | ' %
+            #      (self.__alarm.id_, self.getName(), self.__alarm.hour,
+            #       self.__alarm.minute))
+
+    def stop(self):
+        """
+        Stops the infinite loop in run method and causes the thread to exit once
+        the current operation finishes.
+        """
+        self.__run = False
+
+    #
+    # member methods
+    #
+    def get_id(self):
+        """
+        :return: The AlarmItem id, which is also used to identified this thread.
+        """
+        return self.__id
+
+    def edit_alarm(self, alarm_item):
+        """
+        Edits the alarm instance to a new one, if the IDs are the same.
+        This could be unnecessary, as the input alarm is probably referencing
+        the same instance, but there could be a case where a new AlarmItem
+        instance is created to represent the same alarm.
+        :param alarm_item: AlarmItem for the new Alarm instance.
+        :return: Boolean indicating if the operation was successful.
+        """
+        # Only edit the alarm if it contains the same ID
+        if alarm_item.id_ == self.__id == self.__alarm.id_:
+            self.__alarm = alarm_item
+            sucess = True
+        else:
+            print('ERROR: Provided AlarmItem is not correct for this thread.\n'
+                  + 'Provided Alarm ID: %s  |  Thread Alarm ID: %s !' %
+                  (alarm_item.id_, self.__alarm.id_), file=sys.stderr)
+            sucess = False
+        return sucess
---
+++ LightUpAlarm/AlarmThread_test.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python2
+#
+# Unit test for the AlarmThread class.
+#
+# Copyright (c) 2015 carlosperate https://github.com/carlosperate/
+# Licensed under The MIT License (MIT), a copy can be found in the LICENSE file
+#
+# This only tests the class methods in the current main thread. The Threading
+# aspect is tested in the AlarmManager Unit test when the methods responsible
+# to launch, stop and track the threads are tested.
+#
+from __future__ import unicode_literals, absolute_import
+import io
+import time
+import mock
+import unittest
+from LightUpAlarm.AlarmItem import AlarmItem
+from LightUpAlarm.AlarmThread import AlarmThread
+
+
+class AlarmThreadTestCase(unittest.TestCase):
+    """ Tests for AlarmThread class. """
+    @staticmethod
+    def empty_callback():
+        pass
+
+    def test_constructor(self):
+        """ Tests the class constructor. """
+
+        alarm_test = AlarmItem(
+            9, 34, (True, True, True, True, True, True, True), False, 96)
+        alarm_thread = AlarmThread(alarm_test, AlarmThreadTestCase.empty_callback)
+        self.assertIs(alarm_thread._AlarmThread__alarm, alarm_test)
+        self.assertIs(
+            alarm_thread._AlarmThread__alarm_callback,
+            AlarmThreadTestCase.empty_callback)
+
+    def test_get_id(self):
+        """ Tests the get_id() method. """
+        alarm_test = AlarmItem(
+            9, 34, (True, True, True, True, True, True, True), False, 36)
+        alarm_thread = AlarmThread(
+            alarm_test, AlarmThreadTestCase.empty_callback)
+        self.assertEqual(alarm_thread.get_id(), alarm_test.id_)
+
+    def test_edit_alarm(self):
+        """ Tests the edit_alarm() method. """
+        alarm_test = AlarmItem(
+            9, 34, (True, True, True, True, True, True, True), False, 36)
+        new_alarm = AlarmItem(
+            9, 34, (True, True, True, True, True, True, True), False, 96)
+        alarm_thread = AlarmThread(
+            alarm_test, AlarmThreadTestCase.empty_callback)
+        self.assertIs(alarm_thread._AlarmThread__alarm, alarm_test)
+        # The edit_alarm method prints to stderr if wrong alarm if input, so
+        # we need to capture stderr to test it.
+        with mock.patch('sys.stderr', new=io.StringIO()) as test_srderr:
+            # Test with wrong ID
+            self.assertEqual(test_srderr.getvalue(), '')
+            alarm_thread.edit_alarm(new_alarm)
+            self.assertNotEqual(test_srderr.getvalue(), '')
+            self.assertIsNot(alarm_thread._AlarmThread__alarm, new_alarm)
+            # Test with correct ID
+            new_alarm.id_ = 36
+            test_srderr.truncate(0)
+            test_srderr.write('')
+            self.assertEqual(test_srderr.getvalue(), '')
+            alarm_thread.edit_alarm(new_alarm)
+            self.assertEqual(test_srderr.getvalue(), '')
+            self.assertIs(alarm_thread._AlarmThread__alarm, new_alarm)
+
+    def test_run(self):
+        """
+        Creates and alarm to trigger within a minute to instate AlarmThread.
+        This test uses the member method AlarmManagerTestCase.c as the callback
+        method. As the expected number of arguments is not 0, a TypeError is
+        raised when executed, proving that the run method triggers on the alarm
+        time and executes the callback.
+        This test can take a little over 10 seconds in its worse case scenario.
+        """
+        def callback(one, two, three):
+            """
+            Does nothing, but because it's it expects several argument it will
+            throw an exception if they are not provided.
+            """
+            pass
+        # Set alarm for current minute, but only if there is at least 10seconds
+        # left for this minute. Better than setting it for the next minute.
+        time_now = time.localtime(time.time())
+        while time_now.tm_sec > 50:
+            time.sleep(1)
+            time_now = time.localtime(time.time())
+        alarm_thread = AlarmThread(
+            AlarmItem(
+                time_now.tm_hour, time_now.tm_min,
+                (True, True, True, True, True, True, True), True, 96),
+            callback)
+        self.assertRaises(TypeError, alarm_thread.run)
+
+
+if __name__ == '__main__':
+    unittest.main()
