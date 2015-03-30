1d36605815dbf6ae83b1913bf7f727494161cee2
carlosperate <carlosperate@users.noreply.github.com>
2015-03-30 20:25:36 +0100

    Add to the LightUpServer GET operation for alarms
    Now posible to get, add, edit and delete alarms using get variables in
    the following addresses:
    /LightUpPi/getAlarm?...
    /LightUpPi/addAlarm?...
    /LightUpPi/editAlarm?...
    /LightUpPi/deleteAlarm?...

--- LightUpServer/Server.py
+++ LightUpServer/Server.py
@@ -2,7 +2,8 @@
 #
 # Server component from the LightUpServer package.
 #
-# Copyright (c) 2015 carlosperate https://github.com/carlosperate/
+# Copyright (c) 2015 carlosperate http://carlosperate.github.io
+#
 # Licensed under The MIT License (MIT), a copy can be found in the LICENSE file
 #
 # Longer description.
@@ -29,8 +30,9 @@ alarm_adapt = None
 
 @flask_server.route('/')
 def root_index_redirect():
-    """ Redirects the LightUpPi dir directly to index.html """
+    """ Redirects the LightUpPi dir directly to /LightUpPi/ """
     return redirect('/LightUpPi/', code=302)
+
 
 @flask_server.route('/LightUpPi/')
 def lightuppi_index_redirect():
@@ -39,50 +41,294 @@ def lightuppi_index_redirect():
         flask_server.static_folder, 'index.html')
 
 
-@flask_server.route('/LightUpPi/test')
+@flask_server.route('/LightUpPi/ping')
+def ping_test():
+    """
+    This is used to check if the server is up and accessible, almost like a
+    ping.
+    :return: Empty string with a 200 response.
+    """
+    return ''
+
+@flask_server.route('/LightUpPi/templateTest')
 def template_test():
     """
     This is for the main.html static page entry point.
-    :return:
+    :return: html page from the templates folder.
     """
     now = datetime.datetime.now()
     timeString = now.strftime('%Y-%m-%d %H:%M')
     templateData = {
-        'title' : 'HELLO!',
+        'title': 'HELLO!',
         'time': timeString
     }
     return render_template('main.html', **templateData)
 
 
-@flask_server.route('/LightUpPi/alarms')
-def get_all_alarms_json():
+@flask_server.route('/LightUpPi/getAlarm', methods=['GET'])
+def get_alarm():
     global alarm_adapt
-    alarms_json = alarm_adapt.json_get_all_alarms()
-    return Response(alarms_json,  mimetype='application/json')
+    message = {'error': 'The \'id\' argument is required for \'getAlarm\''}
+    alarm_id = request.args.get('id')
+    if alarm_id is not None:
+        if alarm_id == 'all':
+            # /LightUpPi/getAlarm?id=all
+            json_response = alarm_adapt.json_get_all_alarms()
+            return Response(json_response,  mimetype='application/json')
+        else:
+            # /LightUpPi/getAlarm?id=<alarm_id>
+            try:
+                alarm_id = int(alarm_id)
+                json_response = alarm_adapt.json_get_alarm(int(alarm_id))
+                return Response(json_response,  mimetype='application/json')
+            except ValueError:
+                message['error'] = 'The \'id\' argument has to be an integer'
 
-
-@flask_server.route('/LightUpPi/alarm', methods=['GET'])
-def alarm_operations():
-    global alarm_adapt
-    message = {'message': 'nothing'}
-    url_args = request.args.get('action')
-    if url_args == 'get':
-        alarm_id = int(request.args.get('id'))
-        alarm_json = alarm_adapt.json_get_alarm(alarm_id)
-        return Response(alarm_json,  mimetype='application/json')
-    elif url_args == 'add':
-        message['action'] = 'add'
-    elif url_args == 'edit':
-        message['action'] = 'edit'
-    elif url_args == 'delete':
-        message['action'] = 'delete'
-    else:
-        message['error'] = 'something'
-
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
+    # At this point credentials were invalid or request method was not GET
     return jsonify(message)
-    #return Response(json.dumps(message),  mimetype='application/json')
+
+
+@flask_server.route('/LightUpPi/addAlarm', methods=['GET'])
+def add_alarm():
+    """
+    Adding an alarm to the database and launching it if active.
+    The hour and minute arguments are mandatory and the others are optional.
+    The full request is:
+    /LightUpPi/addAlarm?hour=<>&minute=<>&monday=<>&tuesday=<>&wedneday=<>
+        &thursday=<>&friday=<>&saturday=<>&sunday=<>&enabled=<>&label=<>
+        &timestamp=<>
+    :return: JSON string with response data indicating success of operation.
+    """
+    global alarm_adapt
+
+    # Parsing the hour argument, if not present or wrong return error message
+    hour = request.args.get('hour')
+    if hour is None:
+        message = {'error': 'The \'hour\' argument is required to add alarm'}
+        return jsonify(message)
+    else:
+        try:
+            hour = int(hour)
+        except ValueError:
+            message = {'error': 'The \'hour\' argument must be an integer'}
+            return jsonify(message)
+
+    # Parsing the minute argument, if not present or wrong return error message
+    minute = request.args.get('minute')
+    if minute is None:
+        message = {'error': 'The \'minute\' argument is required to add alarm'}
+        return jsonify(message)
+    else:
+        try:
+            minute = int(minute)
+        except ValueError:
+            message = {'error': 'The \'minute\' argument must be an integer'}
+            return jsonify(message)
+
+    def check_boolean(arg_str):
+        """
+        Internal function to simplify boolean arguments parsing.
+        Set to True by default.
+        """
+        arg_value = request.args.get(arg_str)
+        if arg_value is None:
+            parse_success = True
+            return_value = True
+        elif arg_value.lower() in ("true", "yes", "enabled"):
+            parse_success = True
+            return_value = True
+        elif arg_value.lower() in ("false", "no", "disabled"):
+            parse_success = True
+            return_value = False
+        else:
+            parse_success = False
+            return_value = 'The \'%s\' argument has to be a bool' % arg_str
+        return parse_success, return_value
+
+    # Parsing the weekdays
+    success, monday = check_boolean("monday")
+    if success is False:
+        return jsonify({'error': monday})
+    success, tuesday = check_boolean("tuesday")
+    if success is False:
+        return jsonify({'error': tuesday})
+    success, wednesday = check_boolean("wednesday")
+    if success is False:
+        return jsonify({'error': wednesday})
+    success, thursday = check_boolean("thursday")
+    if success is False:
+        return jsonify({'error': thursday})
+    success, friday = check_boolean("friday")
+    if success is False:
+        return jsonify({'error': friday})
+    success, saturday = check_boolean("saturday")
+    if success is False:
+        return jsonify({'error': saturday})
+    success, sunday = check_boolean("sunday")
+    if success is False:
+        return jsonify({'error': sunday})
+
+    # Parsing the enabled argument, if not present or wrong return error message
+    success, enabled = check_boolean("enabled")
+    if success is False:
+        return jsonify({'error': enabled})
+
+    # Parsing the label argument
+    label = request.args.get('label')
+    if label is None:
+        label = ''
+
+    # Parsing the timestamp argument, if wrong data type return error
+    timestamp = request.args.get('timestamp')
+    if timestamp is not None:
+        try:
+            timestamp = long(minute)
+        except ValueError:
+            message = {'error': 'The \'timestamp\' argument must be an integer'}
+            return jsonify(message)
+
+    # At this point all arguments should be correct
+    json_response = alarm_adapt.json_add_alarm(
+        hour, minute, enabled=enabled, label=label, timestamp=timestamp,
+        days=(monday, tuesday, wednesday, thursday, friday, saturday, sunday))
+    return Response(json_response, mimetype='application/json')
+
+
+@flask_server.route('/LightUpPi/editAlarm', methods=['GET'])
+def edit_alarm():
+    """
+    Edit an alarm to the database and launching it if active.
+    The id arguments is mandatory and the others are optional.
+    The full possible request is:
+    /LightUpPi/addAlarm?id=<>&hour=<>&minute=<>&monday=<>&tuesday=<>
+        &wedneday=<>&thursday=<>&friday=<>&saturday=<>&sunday=<>&enabled=<>
+        &label=<>
+    :return: JSON string with response data indicating success of operation.
+    """
+    global alarm_adapt
+
+    # Parsing the id argument, if not present or wrong return error message
+    id_ = request.args.get('id')
+    if id_ is None:
+        message = {'error': 'The \'id\' argument is required to edit alarm'}
+        return jsonify(message)
+    else:
+        try:
+            id_ = int(id_)
+        except ValueError:
+            message = {'error': 'The \'id\' argument must be an integer'}
+            return jsonify(message)
+
+    # Parsing the hour argument
+    hour = request.args.get('hour')
+    if hour is not None:
+        try:
+            hour = int(hour)
+        except ValueError:
+            message = {'error': 'The \'hour\' argument must be an integer'}
+            return jsonify(message)
+
+    # Parsing the minute argument
+    minute = request.args.get('minute')
+    if minute is not None:
+        try:
+            minute = int(minute)
+        except ValueError:
+            message = {'error': 'The \'minute\' argument must be an integer'}
+            return jsonify(message)
+
+    def check_boolean(arg_str):
+        """ Internal function to simplify boolean arguments parsing. """
+        return_value = request.args.get(arg_str)
+        parse_success = True
+        if return_value is not None:
+            if return_value.lower() in ("true", "yes", "enabled"):
+                parse_success = True
+                return_value = True
+            elif return_value.lower() in ("false", "no", "disabled"):
+                parse_success = True
+                return_value = False
+            else:
+                parse_success = False
+                return_value = 'The \'%s\' argument has to be a bool' % arg_str
+        return parse_success, return_value
+
+    # Parsing the weekdays
+    success, monday = check_boolean("monday")
+    if success is False:
+        return jsonify({'error': monday})
+    success, tuesday = check_boolean("tuesday")
+    if success is False:
+        return jsonify({'error': tuesday})
+    success, wednesday = check_boolean("wednesday")
+    if success is False:
+        return jsonify({'error': wednesday})
+    success, thursday = check_boolean("thursday")
+    if success is False:
+        return jsonify({'error': thursday})
+    success, friday = check_boolean("friday")
+    if success is False:
+        return jsonify({'error': friday})
+    success, saturday = check_boolean("saturday")
+    if success is False:
+        return jsonify({'error': saturday})
+    success, sunday = check_boolean("sunday")
+    if success is False:
+        return jsonify({'error': sunday})
+
+    if any([monday, tuesday, wednesday, thursday, friday, saturday, sunday]):
+        alarm_repeat = list(alarm_adapt.get_alarm_repeat(id_))
+        if monday is not None:
+            alarm_repeat[0] = monday
+        if tuesday is not None:
+            alarm_repeat[1] = tuesday
+        if wednesday is not None:
+            alarm_repeat[2] = wednesday
+        if thursday is not None:
+            alarm_repeat[3] = thursday
+        if friday is not None:
+            alarm_repeat[4] = friday
+        if saturday is not None:
+            alarm_repeat[5] = saturday
+        if sunday is not None:
+            alarm_repeat[6] = sunday
+
+    # Parsing the enabled argument, if not present or wrong return error message
+    success, enabled = check_boolean("enabled")
+    if success is False:
+        return jsonify({'error': enabled})
+
+    # Parsing the label argument
+    label = request.args.get('label')
+
+    # At this point all arguments should be correct
+    json_response = alarm_adapt.json_edit_alarm(
+        alarm_id=id_, hour=hour, minute=minute, enabled=enabled, label=label,
+        days=alarm_repeat)
+    return Response(json_response, mimetype='application/json')
+
+
+@flask_server.route('/LightUpPi/deleteAlarm', methods=['GET'])
+def delete_alarm():
+    global alarm_adapt
+    message = {'error': 'The \'id\' argument is required for \'deleteAlarm\''}
+    alarm_id = request.args.get('id')
+    if alarm_id is not None:
+        if alarm_id == 'all':
+            # /LightUpPi/getAlarm?id=all
+            json_response = alarm_adapt.json_delete_all_alarms()
+            return Response(json_response,  mimetype='application/json')
+        else:
+            # /LightUpPi/getAlarm?id=<alarm_id>
+            try:
+                alarm_id = int(alarm_id)
+                json_response = alarm_adapt.json_delete_alarm(int(alarm_id))
+                return Response(json_response,  mimetype='application/json')
+            except ValueError:
+                message['error'] = 'The \'id\' argument has to be an integer'
+
+    # At this point credentials were invalid or request method was not GET
+    return jsonify(message)
 
 
 def run(alarm_mgr_arg):
