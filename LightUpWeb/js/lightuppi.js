f09739b57f505e58db601890c988b0de903eccb0
carlosperate <carlosperate@users.noreply.github.com>
2015-05-08 18:01:12 +0100

    Add minimal angular base index.html

---
+++ LightUpWeb/js/lightuppi.js
@@ -0,0 +1,8 @@
+/**
+ * @license Licensed under the Apache License, Version 2.0 (the "License"):
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @fileoverview Main LightUpPi app javascript
+ */
+'use strict';
+
---
+++ LightUpWeb/js/lightuppi_controller.js
@@ -0,0 +1,13 @@
+/**
+ * @license Licensed under the Apache License, Version 2.0 (the "License"):
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @fileoverview LightUpPi app AngularJS controller.
+ */
+'use strict';
+
+var lightUpPiApp = angular.module('lightUpPi', ['ui.bootstrap']);
+
+lightUpPiApp.controller('MainCtrl', function($scope) {
+
+});
